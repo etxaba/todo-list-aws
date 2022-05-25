41a87d89f5ae345054f31ea0df45e77bf4759352
etxaba <105485625+etxaba@users.noreply.github.com>
2022-05-25 12:46:04 +0200

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -20,7 +20,8 @@ def get_table(dynamodb=None):
         # fetch todo from the database
         table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
         return table
-    except Exception as e: print(e)
+    except Exception as e: 
+        print(e)
     	
 
 
