1fcc93b561aa42255257eab919e8c0e02989392b
Ubuntu <ubuntu@ip-172-31-90-106.ec2.internal>
2022-06-01 09:51:18 +0000

    Implementada la funcion que me devuelve el texto del todo en el idioma indicado

---
+++ src/translate.py
@@ -0,0 +1,38 @@
+import json
+import decimalencoder
+import todoList
+import boto3
+
+
+def translate(event, context):
+    translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+    
+    # create a response
+    item = todoList.get_item(event['pathParameters']['id'])
+    if item:
+        try:
+            #result = translate.translate_text(Text=item.get('text'), 
+            #        SourceLanguageCode="en", TargetLanguageCode="es")
+            result = translate.translate_text(Text=item.get('text'), 
+                    SourceLanguageCode="auto", TargetLanguageCode=event['pathParameters']['language'])
+            print('TranslatedText: ' + result.get('TranslatedText'))
+            print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
+            print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
+            response = {
+                "statusCode": 200,
+                "body": result.get('TranslatedText')
+            }
+        except Exception as e:
+            print('Estoy en la excepcion')
+            print(e)
+            response = {
+                "statusCode": 400,
+                "body": str(e)#json.dumps(e, cls=decimalencoder.DecimalEncoder)
+            }
+        
+    else:
+        response = {
+            "statusCode": 404,
+            "body": ""
+        }
+    return response
