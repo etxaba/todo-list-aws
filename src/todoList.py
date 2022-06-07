06ab971fda8898ddd5612118d3abb1d8913ea0cb
etxaba <105485625+etxaba@users.noreply.github.com>
2022-06-07 15:30:07 +0200

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -157,17 +157,17 @@ def translate_item(text, language, translate=None):
     try:
         if not translate:
             translate = boto3.client(
-									service_name='translate', 
-									region_name='us-east-1',
-									use_ssl=True)
+                            service_name='translate',
+                            region_name='us-east-1',
+                            use_ssl=True)
         result = translate.translate_text(
-					Text=text,
-					SourceLanguageCode="auto",
-					TargetLanguageCode=language)
+                    Text=text,
+                    SourceLanguageCode="auto",
+                    TargetLanguageCode=language)
         print('TranslatedText: ' + result.get('TranslatedText'))
         print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
         print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
-        
+
         return result
     except Exception as e:
         print(e)
